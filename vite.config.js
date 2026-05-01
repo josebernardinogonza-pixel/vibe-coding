a4b0f1117987e5fc5536b45b29735916090c11e7
BG_DIGITALSOLUTIONS <josebernardinogonza@gmail.com>
2026-05-01 11:03:13 -0600

    Change base path to '/vibe-coding/'
    Update base path for deployment configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  // Esta línea es la que hace que todo funcione
+  base: '/vibe-coding/',
 })
